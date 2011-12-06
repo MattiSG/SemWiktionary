525a345924455edcf18d4d57ee00c2929dd63d4f
ghibaudo <ghibaudo@44478808-8fa7-48f5-8cfd-5cdb2b7c26bb>
2011-12-06 15:14:07 +0000

    Created first source file (src hierarchy).
    git-svn-id: https://redmine.polytech.unice.fr/svn/si5pfe2011-01@13 44478808-8fa7-48f5-8cfd-5cdb2b7c26bb

---
+++ src/edu/unice/polytech/kis/semwiktionary/SemWiktionary.java
@@ -0,0 +1,16 @@
+/**SemWiktionary
+*Java API to access data from [wiktionary](http://fr.wiktionary.org). Specific target is the French wiktionary.
+*
+*@author	[Matti Schneider-Ghibaudo](http://mattischneider.fr)
+*@author	[Fabien Brossier](http://fabienbrossier.fr)
+*@author	Ngoc Nguyen Thinh Dong
+*@author	Steven Sancho
+*
+*@version 0.0.0
+*/
+
+package edu.unice.polytech.kis.semwiktionary;
+
+public class SemWiktionary {
+	
+}

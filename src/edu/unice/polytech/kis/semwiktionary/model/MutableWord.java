4691312e160253e8ae2970b0d2a7e2c6dcfae292
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2011-12-13 11:48:06 +0100

    Improved Word and MutableWord doc.

--- src/edu/unice/polytech/kis/semwiktionary/model/MutableWord.java
+++ src/edu/unice/polytech/kis/semwiktionary/model/MutableWord.java
@@ -1,28 +1,28 @@
 package edu.unice.polytech.kis.semwiktionary.model;
 
+
 import org.neo4j.graphdb.Node;
 
 import edu.unice.polytech.kis.semwiktionary.database.Database;
 import edu.unice.polytech.kis.semwiktionary.database.Relation;
 
-/** Represents a simple word in the database.
- * The class provides Read methods by his parent, in addition to Create, Update, and Delete methods. 
- * Some properties are only loaded when their associated get methods are called.
+
+/** Adds Create, Update, and Delete methods to a Word.
+ * Instances of this class can modify the database. For a safe usage, see `Word`.
  * 
- * @author Fabien BROSSIER
- * @author Matti SCHNEIDER
- * @author Steven SANCHO
- * @author Thinh DONG
- * @version 1.0
+ * @author	[Fabien Brossier](http://fabienbrossier.fr)
+ * @author	[Matti Schneider-Ghibaudo](http://mattischneider.fr)
  */
 public class MutableWord extends Word {
 	
 // STATIC METHODS
 	
-	/** Finds a word in the database from his title.
-	 * Constructs a MutableWorld object with all his properties (definition, synonyms, ...). 
-	 * @param word The title of the world to find in the database
-	 * @return The complete Word object created or null if the word is not in the database (unlikely) 
+	/** Finds a word in the database from its title.
+	 * Constructs a MutableWord object with all its properties (definition, synonyms…).
+	 * Such an object _may_ modify the database. See `Word` for a safer use.
+	 *
+	 * @param	word	The word to model
+	 * @return	The complete MutableWord object created or null if the word is not in the database
 	 */
 	public static MutableWord from(String word) {
 		if (Word.exists(word))
@@ -31,11 +31,11 @@ public class MutableWord extends Word {
 		return null;
 	}
 	
-	/** Creates a new Word in the database from the specified title.
-	 * The word is created even if the same word already exists in the database.
-	 * You should use the functions "from" or "exists" to check the existence before.
-	 * @param word The title of the Word object created
-	 * @return The MultableWord object added to the database
+	/** Creates a new word in the database.
+	 * The word is created even if the same word already exists in the database, possibly leading to duplicates. You should use the `Word.exists` method to check for existence before creating a new word.
+	 *
+	 * @param	word	The natural language word to add to the database
+	 * @return	The MutableWord model for the word added to the database
 	 */
 	public static MutableWord create(String word) {
 		MutableWord result = new MutableWord(word);
@@ -45,8 +45,9 @@ public class MutableWord extends Word {
 	
 // CONSTRUCTORS
 
-	/** Constructs a MutableWord from his title.
-	 * @param title The title of the Word
+	/** Models an editable natural-language word.
+	 *
+	 * @param	word	The natural language word to model
 	 */
 	private MutableWord(String word) {
 		super(word);
@@ -54,10 +55,11 @@ public class MutableWord extends Word {
 	
 	// UPDATE FUNCTIONS
 	
-	/** Adds the specified definition to the current Word.
-	 * The definition is created and linked in the database but also added to the definition list attribute.  
-	 * @param definition The Definition object associated to the Word
-	 * @return The Word which is attached the Definition.
+	/** Adds the given Definition to this Word.
+	 * The definition is immediately and transparently stored in the database. No need to `commit` modifications.
+	 * 
+	 * @param	definition	The Definition to add to this Word
+	 * @return	This MutableWord, for chainability
 	 */
 	public MutableWord addDefinition(Definition definition) {
 		Node defNode = Database.createNodeWithProperty("definition", definition.getDefinition());
@@ -71,10 +73,11 @@ public class MutableWord extends Word {
 		return this;
 	}
 
-	/** Remove a definition from the definition list of the current Word.
-	 * The definition is removed from the database and from the definition list attribute.
-	 * @param definition The definition object to remove
-	 * @return The current word
+	/** Removes the giben Definition from the definitions for this Word.
+	 * The definition is immediately and transparently removed from the database. No need to `commit` modifications.
+	 *
+	 * @param	definition	The definition object to remove
+	 * @return	This MutableWord, for chainability
 	 */
 	public MutableWord removeDefinition(Definition definition) {
 		//TODO database
@@ -85,7 +88,8 @@ public class MutableWord extends Word {
 
 	// DELETE FUNCTIONS
 	
-	/** Deletes a word and all his properties from the database.Z 
+	/** Deletes a word and all of its properties from the database.
+	 *
 	 * @param word
 	 */
 	// TODO define the behavior of the relations 
@@ -94,9 +98,10 @@ public class MutableWord extends Word {
 		//TODO call other delete functions
 	}
 	
-	/** Deletes all definitions of the current Word.
-	 * The definitions are deleted from the database and the attribute list is cleared.
-	 * @return The current word after the suppression of definitions.
+	/** Deletes all definitions for the current Word.
+	 * The definitions are immediately and transparently removed from the database. No need to `commit` modifications.
+	 *
+	 * @return	This MutableWord, for chainability
 	 */
 	public MutableWord clearDefinitions() {
 		//TODO database

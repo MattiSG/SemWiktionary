2b9712414bc4abc65410d0b2b82376df2d4aa59b
Steven Sancho <s.sancho88@gmail.com>
2011-12-14 11:02:21 +0100

    Activated test for the parser. Failed definitions matching (no definition found)

--- test/RunAndPray.java
+++ test/RunAndPray.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	edu.unice.polytech.kis.semwiktionary.database.DatabaseTest.class
+	edu.unice.polytech.kis.semwiktionary.database.DatabaseTest.class,
+        edu.unice.polytech.kis.semwiktionary.parser.ParserTest.class
 })
 
 public class RunAndPray {}

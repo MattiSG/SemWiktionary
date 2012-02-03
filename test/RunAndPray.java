50072352a5824eab9c5471fc751ee3e207846955
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-02-03 21:33:57 +0100

    Improved test ordering to decrease risks of interference.

--- test/RunAndPray.java
+++ test/RunAndPray.java
@@ -11,11 +11,15 @@ import edu.unice.polytech.kis.semwiktionary.database.DatabaseTest;
 @RunWith(Suite.class)
 @SuiteClasses({
 	edu.unice.polytech.kis.semwiktionary.database.DatabaseTest.class,
-	edu.unice.polytech.kis.semwiktionary.parser.ParserTest.class, // after this test suite, data will be stored in database
-	edu.unice.polytech.kis.semwiktionary.model.WordTest.class,
 	edu.unice.polytech.kis.semwiktionary.model.MutableWordTest.class,
 	edu.unice.polytech.kis.semwiktionary.model.DefinitionTest.class,
+	
+	// test suites above this line don't need the database to be filled
+	
+	edu.unice.polytech.kis.semwiktionary.parser.ParserTest.class, // after this test suite, data will be stored in database
+	edu.unice.polytech.kis.semwiktionary.model.WordTest.class,
 	edu.unice.polytech.kis.semwiktionary.model.SynonymsTest.class
+
 	//database cleanup is done in the @AfterClass below
 })
 

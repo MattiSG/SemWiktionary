MattiSG/SemWiktionary

commit e8321f90258973872b890b89bb0b6a93429d0ff9

Simplified DEFINITION parsing and made it more resilient to badly-spaced definitions lists. Passes "chinois".

src/edu/unice/polytech/kis/semwiktionary/parser/WikimediaDump.jflex
@@ -119,7 +119,7 @@ newline = (\r|\n|\r\n)
 optionalSpaces = ({whitespace}*)
 space = ({whitespace}|{newline})
 
-%state TITLE, MEDIAWIKI, LANG, H2, NATURE, SECTION, PATTERN, PRONUNCIATION, DEFINITION, DEFINITION_DOMAIN, DEFINITION_BODY, DEFINITION_EXAMPLE, SIMPLENYM, SPNM_CONTEXT, SPNM_WORD
+%state TITLE, MEDIAWIKI, LANG, H2, NATURE, SECTION, PATTERN, PRONUNCIATION, DEFINITION, DEFINITION_DOMAIN, DEFINITION_EXAMPLE, SIMPLENYM, SPNM_CONTEXT, SPNM_WORD
 
 %xstate PAGE
 
@@ -269,7 +269,7 @@ space = ({whitespace}|{newline})
 		yybegin(PATTERN);
 	}
 
-	{newline}#+
+	{newline}#+{optionalSpaces}
 	{
 		int newDepth = yytext().length();
 		
@@ -339,20 +339,6 @@ space = ({whitespace}|{newline})
 }
 
 
-<DEFINITION> 
-{
-	{optionalSpaces}"{{"
-	{
-		yybegin(DEFINITION_DOMAIN);
-	}
-	
-	{optionalSpaces}
-	{
-		yybegin(DEFINITION_BODY);
-	}
-}
-
-
 <DEFINITION_DOMAIN>
 {
 	[^|}]+
@@ -362,7 +348,7 @@ space = ({whitespace}|{newline})
 	
 	(\|[^}]*)?"}}"{optionalSpaces}
 	{
-		yybegin(DEFINITION_BODY);
+		yybegin(DEFINITION);
 	}
 }
 
@@ -388,9 +374,9 @@ space = ({whitespace}|{newline})
 }
 
 
-<DEFINITION_BODY>
+<DEFINITION>
 {
-	.+
+	[^{]+
 	{
 		definitionsBuffer.add(yytext());
 		
@@ -401,6 +387,11 @@ space = ({whitespace}|{newline})
 		currentDefinition.setContent(result);
 		
 		yybegin(SECTION);
+	}
+	
+	"{{"
+	{
+		yybegin(DEFINITION_DOMAIN);
 	}
 }
 

commit 84b55869d76662d558ba0eea53d2bae3d50efdfc

Corrected definitions parsing (regression introduced in e8321f90258973872b890b89bb0b6a93429d0ff9).

src/edu/unice/polytech/kis/semwiktionary/parser/WikimediaDump.jflex
@@ -389,7 +389,7 @@ space = ({whitespace}|{newline})
 
 <DEFINITION>
 {
-	[^{]+
+	[^\r\n{]+
 	{
 		definitionsBuffer.add(yytext());
 		
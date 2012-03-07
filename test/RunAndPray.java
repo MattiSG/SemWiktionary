6399ff99f64bd8a85448234ed2befa24ee1a084c
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-03-07 09:24:59 +0100

    Added proper JUnit test for HTML entities.

--- test/RunAndPray.java
+++ test/RunAndPray.java
@@ -18,6 +18,7 @@ import edu.unice.polytech.kis.semwiktionary.database.DatabaseTest;
 	
 	edu.unice.polytech.kis.semwiktionary.parser.ParserTest.class, // after this test suite, data will be stored in database
 	edu.unice.polytech.kis.semwiktionary.model.WordTest.class,
+	edu.unice.polytech.kis.semwiktionary.parser.HTMLEntitiesParsingTest.class,
 	edu.unice.polytech.kis.semwiktionary.model.SynonymsTest.class,
 	edu.unice.polytech.kis.semwiktionary.model.AntonymsTest.class,
 	edu.unice.polytech.kis.semwiktionary.model.TroponymsTest.class
